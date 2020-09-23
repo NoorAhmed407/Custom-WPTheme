2cbc0d1e647d79e5716c76fa76198bf5a9fd9def
Noor <engrnoorahmed407@gmail.com>
2020-09-23 08:15:54 -0700

    Updated

--- front-page.php
+++ front-page.php
@@ -2,7 +2,16 @@
 
 
 <div class="container">
-   <h1> <?php the_title();?> </h1>
+
+
+   <h1> 
+     <?php the_title();?> 
+   </h1>
+
+   <?php get_template_part('includes/section', 'content');?>
+
+
+
 </div>
 
 
